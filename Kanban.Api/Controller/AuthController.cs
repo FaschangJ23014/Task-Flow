18e09ebd01f92a317b8a7a63b3eacc398d5680dd
Jakob Faschang <jakob.faschang@schule.at>
2026-08-05 16:56:51 +0200

    Feat: created AuthService and Controller with Hashing and JWT Token

---
+++ Kanban.Api/Controller/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using Kanban.Api.DTOs;
+using Kanban.Api.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kanban.Api.Controller;
+
+[Route("api/auth")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly AuthService authService;
+
+
+    public AuthController(AuthService _authService)
+    {
+        authService = _authService;
+    }
+
+    [HttpPost("register")]
+    public IActionResult Register(UserDto dto)
+    {
+        bool register = authService.Register(dto.Username, dto.Password);
+        if (!register) return BadRequest("Username existiert bereits");
+
+        return Ok("User erfolgreich registriert!");
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login(UserDto dto)
+    {
+        string? token = authService.Login(dto.Username, dto.Password);
+        if (token == null) return BadRequest("Falscher Username oder Passwort.");
+
+        return Ok(new { Token = token });
+    }
+
+
+}
--- Kanban.Api/Services/AuthService.cs
+++ Kanban.Api/Services/AuthService.cs
@@ -1,6 +1,84 @@
-﻿namespace Kanban.Api.Services
+﻿using Kanban.Api.Data;
+using Kanban.Api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Kanban.Api.Services;
+
+public class AuthService
 {
-    public class AuthService
+    private readonly PasswordHasher<User> _hasher = new();
+    private readonly IConfiguration _config;
+    private readonly DataContext _data;
+
+    public AuthService(IConfiguration config, DataContext data)
     {
+        _config = config;
+        _data = data;
+    }
+
+    public string CreateToken(User user)
+    {
+        // 1. Claims: Das sind die Informationen, die im Token stecken sollen
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Name, user.Username)
+        };
+
+        // 2. Secret Key: Der wird aus der Konfiguration gelesen(Render Environment)
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+            _config["JWT:SecretKey"] ?? "DiesIstEinStandardKeyDerNurZumTestenDient"));
+
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        // 3. Token zusammenbauen
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.Now.AddDays(1), // Token ist 1 Tag gültig
+            SigningCredentials = creds
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
+    }
+
+    public string HashPassword(User user, string password)
+        => _hasher.HashPassword(user, password);
+
+    public bool VerifyPassword(User user, string hashedPassword, string providedPassword)
+        => _hasher.VerifyHashedPassword(user, hashedPassword, providedPassword) == PasswordVerificationResult.Success;
+
+    public bool Register(string username, string password)
+    {
+        if(_data.Users.Any(x => x.Username == username)) return false;
+
+        var user = new User
+        {
+            Username = username
+        };
+
+        user.PasswordHash = HashPassword(user, password);
+
+        _data.Users.Add(user);
+        _data.SaveChanges();
+        return true;
+    }
+
+    public string? Login(string username, string password)
+    {
+        var user = _data.Users.FirstOrDefault(x => x.Username == username);
+        if (user == null) return null;
+
+        bool isPasswordValid = VerifyPassword(user, user.PasswordHash, password);
+        if(isPasswordValid == false) return null;
+
+        return CreateToken(user);
     }
 }
