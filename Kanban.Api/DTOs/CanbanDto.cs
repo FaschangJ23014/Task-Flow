de9035d773e2b0bd8e08e035b7e0eeb5997c7c31
FaschangJ23014 <FaschangJ23014@sus.htl-grieskirchen.at>
2026-08-15 19:20:04 +0200

    Fix: Dto fixed

--- Kanban.Api/DTOs/CanbanDto.cs
+++ Kanban.Api/DTOs/CanbanDto.cs
@@ -8,9 +8,6 @@ namespace Kanban.Api.DTOs
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string Status { get; set; } = "Todo";
-
-        public int UserId { get; set; }
-
         public int? TeamId { get; set; }
     }
 }
